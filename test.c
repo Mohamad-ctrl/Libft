5ef00abbb7a5087a910f26f779b065f838669237
Mohamad Sharif <mohamad99sharif@gmail.com>
2022-07-18 23:02:01 +0400

    test.c

--- test.c
+++ test.c
@@ -42,8 +42,43 @@
 // 	ft_toupper_test();
 // 	return 0;
 // }
+// int main(int argc, char **argv)
+// {
+// 	int array[3][4] = {{1, 2, 3, 4, 5}, {10, 20, 30, 40, 50}, {100, 200, 300, 400, 500}, {1000, 2000, 3000, 4000, 5000}};
+// 	printf("%d\n", &array[0][2]);
+// }
+
+// int game(int gu)
+// {
+// 	int secnum = 100;
+// 	if(gu != secnum)
+// 	{
+// 		printf("Wrong guess bud\n");
+// 		return (0);
+// 	}
+// 	if (gu == secnum)
+// 	{
+// 		printf("Congrats you won a car :D\n");
+// 		return (0);
+// 	}
+// 	else
+// 		printf("Error\n");
+// 	return (0);
+// }
+
+// int main(int argc, char **argv)
+// {
+// 	if (argc == 2)
+// 	{
+// 		game(ft_atoi(argv[1]));
+// 	}
+// 	return 0;
+// }
+
 int main(int argc, char **argv)
 {
-	int array[3][4] = {{1, 2, 3, 4, 5}, {10, 20, 30, 40, 50}, {100, 200, 300, 400, 500}, {1000, 2000, 3000, 4000, 5000}};
-	printf("%d\n", &array[0][2]);
+	int *ptr;
+
+	printf("%p\n", &ptr);
+	return 0;
 }
