bca88c61666c87dfd582e2c8679e51d999daee73
Mohamad Sharif <mohamad99sharif@gmail.com>
2022-08-04 03:09:29 +0400

    did ft_itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -11,8 +11,52 @@
 /* ************************************************************************** */
 #include "libft.h"
 
+int lencount(int x)
+{
+    size_t i;
+
+    if (x == 0)
+        return (1);
+    i = 0;
+    if (x < 0)
+        i++;
+    while (x)
+    {
+        x /= 10;
+        i++;
+    }
+    return(i);
+}
+
 char *ft_itoa(int n)
 {
+    int len;
+    int i;
     char *res;
+
+    len = lencount(n);
+    res = (char)malloc(len + 1);
+    if (!res)
+        return (NULL);
+    res[len] = '\0';
+    if (n < 0)
+    {
+        res[0] = '-';
+        i = 1;
+    }
+    else
+        i = 0;
+    while (len-- > 1)
+        if (n < 0)
+        {
+            res[len] = '0' + n % 10;
+            n = n/ 10;
+        }
+        else
+        {
+            res[len] = '0' + n % 10;
+            n = n / 10;
+        }
+        return (res);
 }
 
